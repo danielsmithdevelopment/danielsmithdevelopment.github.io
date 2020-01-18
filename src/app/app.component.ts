95b9f4a6c04f9b814cd6d31cc60cafb1c6de907b
Daniel Smith <danielsmithdevelopment@gmail.com>
2020-01-17 20:25:14 -0800

    update site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnDestroy {
+  title = 'Index Manager';
+  appStatus = 'running';
+  pdfSrc = 'https://drive.google.com/open?id=1bpTixq_1MkZF1apPeTAltkdDc8Nwaiun';
+
+  private twitter: any;
+
+  constructor(private _router: Router) {
+    this.initTwitterWidget();
+  }
+
+  initTwitterWidget() {
+    this.twitter = this._router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        (<any>window).twttr = (function (d, s, id) {
+          let js: any, fjs = d.getElementsByTagName(s)[0],
+              t = (<any>window).twttr || {};
+          if (d.getElementById(id)) return t;
+          js = d.createElement(s);
+          js.id = id;
+          js.src = "https://platform.twitter.com/widgets.js";
+          fjs.parentNode.insertBefore(js, fjs);
+
+          t._e = [];
+          t.ready = function (f: any) {
+              t._e.push(f);
+          };
+
+          return t;
+        }(document, "script", "twitter-wjs"));
+
+        if ((<any>window).twttr.ready())
+          (<any>window).twttr.widgets.load();
+
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.twitter.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
